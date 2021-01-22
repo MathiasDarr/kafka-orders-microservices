2b31a3022f5d8677a9e072537adce8f57864af64
mddarr <mddarr@gmail.com>
2021-01-21 18:47:38 -0800

    endpoints /api/orders are restricted to authenticated users.. Pass through the /api/products

--- microservices/ui-request-service/src/main/java/org/mddarr/uirequestservice/config/SecurityConfig.java
+++ microservices/ui-request-service/src/main/java/org/mddarr/uirequestservice/config/SecurityConfig.java
@@ -1,4 +1,20 @@
 package org.mddarr.uirequestservice.config;
 
-public class SecurityConfig {
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/api/products/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+    }
+
 }
